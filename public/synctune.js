f213e408ae0d183b00b6f10423bf7485abdf1765
Lux <lxw404@gmail.com>
2020-09-01 23:40:07 -0400

    Fixed various command queue locking issues.
    Fixed YouTube player widget event handling such that only responded events cause dequeuing.  Fixed an issue with invalid commands causing lockup due to not being responded to.  Fixed an issue with the SoundCloud widget not responding to consecutive pause commands due to state change not triggering.  Fixed an issue with the YouTube widget not responding to consecutive play/pause commands due to state change not triggering.  Fixed an issue wherein if no player has been selected and a command is issued, the command queue would lock.

--- public/synctune.js
+++ public/synctune.js
@@ -12,20 +12,22 @@ var SC = require('soundcloud');
 const MAX_RETRY = 5;      // Maximum number of retries
 const RETRY_DELAY = 5000; // Retry delay (ms)
 const POLL_DELTA = 1000;  // Poll delta (ms)
+const WATCHDOG_DELAY = 10000; // Delay for anti-locking mechanism
 
 // Globals
 //var rawList = []; // Current raw list of gallery elements
-var widget = {};   // Widget
-var playlist = []; // Current playlist
-var jn = 0;        // Current request number
+var widget_sc = {}; // Soundcloud widget
+var widget_yt = {}; // YouTube widget
+var wt = '';        // Current widget type
+var playlist = [];  // Current playlist
+var jn = 0;         // Current request number
 var cmdQueue = ['load']; // Queue of commands
+window.cmdQueue = cmdQueue;
 
 // Is an element contained within two index boundaries?
 function conT(e, ind, end){
     return ((ind > e.ind) && (end < e.end));
 }
-
-
 
 // Request link (with retry)
 function reqLink(linkName, i){
@@ -52,25 +54,74 @@ function reqLink(linkName, i){
 }
 
 function execCmd(cmd){
+    var valid = false;
+    
     if (cmd == "play"){
+        valid = true;
         console.log("PLAY");
-        widget.play();
+        if (wt == 'yt'){
+            var st = widget_yt.getPlayerState();
+            if (st == 1){
+                console.log("Already playing.");
+                cmdQueue.shift();
+            }
+            else {
+                console.log("TRY PLAY");
+                widget_yt.playVideo();
+            }
+        }
+        else if (wt == 'sc'){
+            widget_sc.play();
+        }
+        else {
+            console.error("No player to play.");
+            cmdQueue.shift();
+        }
     }
     else if (cmd == "pause"){
+        valid = true;
         console.log("PAUSE");
-        widget.pause();
+        if (wt == 'yt'){
+            var st = widget_yt.getPlayerState();
+            if (st != 1){
+                console.log("Already paused.");
+                cmdQueue.shift();
+            }
+            else {
+                widget_yt.pauseVideo();
+            }
+        }
+        else if (wt == 'sc'){
+            widget_sc.isPaused(function(res){
+                if (res){
+                    console.log("Already paused.");
+                    cmdQueue.shift();
+                }
+                else {
+                    widget_sc.pause();
+                }
+            });
+        }
+        else {
+            console.error("No player to pause.");
+            cmdQueue.shift();
+        }
     }
     else {
         // Regular expression check
-        var pat = /load https*:\/\/soundcloud.com(\/[\S]+)/g;
-        //var mat = cmd.match(pat);
-        var mat = pat.exec(cmd);
-        //console.log(mat);
-        //console.log(mat.length);
-        if (mat != null){
-            if (mat.length == 2){
-                console.log("LOAD:" + mat[1]);
-                widget.load(mat[1], {
+        var pat1 = /load https?:\/\/(?:www\.)?soundcloud\.com(\/[\S]+)/g;
+        var pat2 = /load https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([\S]{11})/g;
+        var mat1 = pat1.exec(cmd);
+        var mat2 = pat2.exec(cmd);
+        if (mat1 != null){
+            if (mat1.length == 2){
+                valid = true;
+                console.log("LOAD SC: " + mat1[1]);
+                if (wt == 'yt'){
+                    widget_yt.stopVideo();
+                }
+                wt = 'sc'; // Switch widget type
+                widget_sc.load(mat1[1], {
                     callback: function(e){
                         console.log("LOADED");
                         cmdQueue.shift();
@@ -81,17 +132,44 @@ function execCmd(cmd){
                 });
             }
         }
+        else if (mat2 != null){
+            if (mat2.length == 2){
+                valid = true;
+                console.log("LOAD YT: " + mat2[1]);
+                if (wt == 'sc'){
+                    widget_sc.pause();
+                }
+                wt = 'yt'; // Switch widget type
+                widget_yt.cueVideoById(mat2[1]);
+            }
+        }
         else {
-            pat = /seek ([\d]+)/g;
+            var pat = /seek ([\d]+)/g;
             mat = pat.exec(cmd);
-            console.log(mat);
+            //console.log(mat);
             if (mat != null){
                 if (mat.length == 2){
+                    valid = true;
                     console.log("SEEK: " + mat[1]);
-                    widget.seekTo(parseInt(mat[1], 10));
+                    if (wt == 'yt'){
+                        widget_yt.seekTo(parseFloat(mat[1], 10)/1000.0);
+                    }
+                    else if (wt == 'sc'){
+                        widget_sc.seekTo(parseInt(mat[1], 10));
+                    }
+                    else {
+                        console.error("No player to seek.");
+                        cmdQueue.shift();
+                    }
                 }
             }
         }
+    }
+    
+    // Remove command from queue if invalid
+    if (!valid){
+        cmdQueue.shift();
+        console.error("INVALID COMMAND");
     }
 }
 
@@ -118,7 +196,7 @@ function onT(){
                 //console.log(JSON.stringify(data));
                 
                 // Check command
-                if (('cmd' in data) && !($.isEmptyObject(widget))){
+                if (('cmd' in data) && !($.isEmptyObject(widget_sc)) && !($.isEmptyObject(widget_yt))){
                     var cmd = data['cmd'];
                     tryCmd(cmd);
                 }
@@ -132,7 +210,49 @@ var si = setInterval(onT, POLL_DELTA);
 
 // On document load, add content
 $(document).ready(function(){
-    // Initialize player
+    // Initialize YT player
+    var tag = document.createElement('script');
+    tag.id = 'yt-iframe-api';
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window.onYouTubeIframeAPIReady = function() {
+        widget_yt = new YT.Player('player-yt', {
+            events: {
+                'onReady': function(e){
+                    console.log("READY YT");
+                    console.log(e.target == widget_yt);
+                },
+                'onStateChange': function(e){
+                    console.log(e);
+                    if ([0,1,2,5].indexOf(e.data) > -1){
+                        if (e.data == 0){
+                            // Finished
+                            console.log("FINISHED YT");
+                        }
+                        else if (e.data == 1){
+                            // Playing
+                            console.log("PLAYING YT");
+                        }
+                        else if (e.data == 2){
+                            // Paused
+                            console.log("PAUSED YT");
+                        }
+                        else if (e.data == 5){
+                            // Cued
+                            console.log("CUED YT");
+                        }
+                        cmdQueue.shift();
+                        if (cmdQueue.length > 0){
+                            execCmd(cmdQueue[0]);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    // Initialize SC player
     SC.initialize({
         client_id: 'Client ID'
     });
@@ -143,44 +263,44 @@ $(document).ready(function(){
     /*SC.stream(track_url).then(function(player){
         player.play();
     });*/
-    var ifr = document.querySelector('iframe');
-    widget = SC.Widget(ifr);
+    var ifr_sc = document.querySelector('#player-sc');
+    widget_sc = SC.Widget(ifr_sc);
     
-    // Bind player events
-    widget.bind(SC.Widget.Events.READY, function(e){
-        console.log("READY");
+    // Bind SC player events
+    widget_sc.bind(SC.Widget.Events.READY, function(e){
+        console.log("READY SC");
         cmdQueue.shift();
         if (cmdQueue.length > 0){
             execCmd(cmdQueue[0]);
         }
     });
     
-    widget.bind(SC.Widget.Events.PLAY, function(e){
-        console.log("PLAYING");
+    widget_sc.bind(SC.Widget.Events.PLAY, function(e){
+        console.log("PLAYING SC");
         cmdQueue.shift();
         if (cmdQueue.length > 0){
             execCmd(cmdQueue[0]);
         }
     });
     
-    widget.bind(SC.Widget.Events.PAUSE, function(e){
-        console.log("PAUSED");
+    widget_sc.bind(SC.Widget.Events.PAUSE, function(e){
+        console.log("PAUSED SC");
         cmdQueue.shift();
         if (cmdQueue.length > 0){
             execCmd(cmdQueue[0]);
         }
     });
     
-    widget.bind(SC.Widget.Events.SEEK, function(e){
-        console.log("SEEKED");
+    widget_sc.bind(SC.Widget.Events.SEEK, function(e){
+        console.log("SEEKED SC");
         cmdQueue.shift();
         if (cmdQueue.length > 0){
             execCmd(cmdQueue[0]);
         }
     });
     
-    widget.bind(SC.Widget.Events.FINISH, function(e){
-        console.log("FINISHED");
+    widget_sc.bind(SC.Widget.Events.FINISH, function(e){
+        console.log("FINISHED SC");
         cmdQueue.shift();
         if (cmdQueue.length > 0){
             execCmd(cmdQueue[0]);
